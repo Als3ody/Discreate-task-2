9cd479e5bd8a092cebd48f884ad66a0834f82425
Als3ody <112738153+Als3ody@users.noreply.github.com>
2022-12-29 17:11:23 +0200

    Add files via upload

---
+++ Perfect/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number 1");
+        int a =int.Parse(Console.ReadLine());
+        Console.WriteLine("Enterr the sec number ");
+        int b =int.Parse(Console.ReadLine());
+        for(; a<=b; a++)
+        {
+            int c= 0;
+            for (int n =1 ; n<a; n++)
+            {
+                if (a%n==0)
+                {
+                    c += n;
+                }
+            }
+            if (c == a)
+            {
+                Console.WriteLine(a);
+            }
+        }
+        Console.ReadKey();  
+    }
+}
